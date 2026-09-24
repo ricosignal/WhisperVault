f6de4d430ca0a75d861e7517c33a5b4640b7a1c2
ricosignal <mcwsailorman@gmail.com>
2026-09-24 13:27:51 -0400

    Refresh app cache after age gate fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='whispervault-v3-1';
+const CACHE='whispervault-v3-2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
