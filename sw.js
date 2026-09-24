8c57bda7f0a635bdaa2afaf7be8ad7ccaf506169
ricosignal <mcwsailorman@gmail.com>
2026-09-24 15:23:27 -0400

    Finalize video discovery deployment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='whispervault-v6-2';
+const CACHE='whispervault-v6-3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg','./soundgasm-catalog.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
